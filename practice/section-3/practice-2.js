25c66abaf7845b8d1aa2ea79590b7474b8c62452
mn <1490949269@qq.com>
2019-08-19 19:54:50 +0800

    作业

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var words = [];
+  var num = [];
+
+  function find(arrA, arrB, words, num) {
+    for (var i = 0; i < arrA.length; i++) {
+      for (var j = 0; j < arrB.value.length; j++) {
+        if (arrA[i].key === arrB.value[j]) {
+          words.push(arrA[i].key);
+          num.push(arrA[i].count - parseInt(arrA[i].count / 3));
+          break;
+        }
+      }
+      if (j === arrB.value.length) {
+        words.push(arrA[i].key);
+        num.push(arrA[i].count)
+      }
+    }
+  }
+
+  find(collectionA, objectB, words, num);
+  var answer = [];
+  for (var k = 0; k < words.length; k++) {
+    answer.push({key: words[k], count: num[k]});
+  }
+  return answer;
 }
--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,83 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  function find(arr, word) {
+    var ret = -1;
+    for (var i = 0; i < arr.length; i++) {
+      if (word === arr[i]) {
+        ret = i;
+      }
+    }
+    return ret;
+  }
+
+  function search(arr, word, count) {
+    for (var i = 0; i < arr.length; i++) {
+      var position = find(word, arr[i][0]);
+      if (arr[i].length != 1) {
+        if (position === -1) {
+          word.push(arr[i][0]);
+          count.push(Number(arr[i][2]));
+        }
+        else {
+          count[position] += Number(arr[i][2]);
+        }
+      }
+      else {
+        if (position === -1) {
+          word.push(arr[i]);
+          count.push(1);
+        }
+        else {
+          count[position]++;
+        }
+      }
+    }
+  }
+
+  function getarrC() {
+    var answer = [];
+    for (var j = 0; j < words.length; j++) {
+      answer.push({key: words[j], count: num[j]});
+    }
+    return answer;
+  }
+
+  function output() {
+    var answer = [];
+    for (var k = 0; k < words1.length; k++) {
+      answer.push({key: words1[k], count: num1[k]});
+    }
+    return answer;
+  }
+
+  function match(arrC, objectB, words1, num1) {
+    for (var i = 0; i < arrC.length; i++) {
+      for (var j = 0; j < objectB.value.length; j++) {
+        if (arrC[i].key === objectB.value[j]) {
+          words1.push(arrC[i].key);
+          num1.push(arrC[i].count - parseInt(arrC[i].count / 3));
+          break;
+        }
+      }
+      if (j === objectB.value.length) {
+        words1.push(arrC[i].key);
+        num1.push(arrC[i].count);
+      }
+    }
+  }
+
+  var words = [];
+
+  var num = [];
+  var arrC = [];
+
+
+  var words1 = [];
+
+  var num1 = [];
+  search(collectionA, words, num);
+  arrC = getarrC();
+  match(arrC, objectB, words1, num1);
+  return output();
 }
